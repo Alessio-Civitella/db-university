dbb4b7f5b2466e172732df73497cc6c5747a998e
alessio-civitella <alessio.civitella19@gmail.com>
2025-01-20 17:08:50 +0100

    Feat: es query

---
+++ query.sql
@@ -0,0 +1,23 @@
+SELECT * FROM db_university.students
+WHERE year(date_of_birth) = 1990;
+
+SELECT * FROM db_university.courses
+WHERE cfu > 10
+
+SELECT * 
+FROM db_university.students
+WHERE TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) > 30;
+
+SELECT * FROM db_university.courses
+WHERE year = 1 AND period LIKE 'I sem%'
+
+SELECT * FROM db_university.exams
+WHERE hour > '14:00:00' AND date = '2020-06-20'
+
+SELECT * FROM db_university.degrees
+WHERE LIKE 'mag%'
+
+SELECT COUNT() FROM db_university.departments
+
+SELECT FROM db_university.teachers
+WHERE phone IS NULL
